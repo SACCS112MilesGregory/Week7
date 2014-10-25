04b9c83a479b063f021200b2cbe8c11e66b8353f
Gregory Miles <milesg_99@yahoo.com>
2014-10-25 01:32:50 -0700

    Week7Lab3 Implementation

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,7 +1,18 @@
+import java.util.Random;
+
 public class RandomNumber 
 { 
+	Random randomNumber = new Random();
+	
+	
 	public int GetANumber_Between_1_and_3()
 	{
+		return (int)(Math.random() * 3 + 1);
+	}
+	
+	public int GetANumber(int lo, int hi)
+	{
+					
 		return (int)(Math.random() * 3 + 1);
 	}
 }
